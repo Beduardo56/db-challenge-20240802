82e8f1c23eb2f2057187017618f8122802f94f0a
Bruno Eduardo <bruno.freire@timeenergy.com.br>
2024-09-11 19:39:57 -0300

    Implementação da consulta que busca quais produtos ainda não foram vendidos.

---
+++ sqls/second_question.sql
@@ -0,0 +1,14 @@
+-- Listar os Produtos que não tenham sido comprados
+
+SELECT
+    products.product_id,
+    products.product_name AS "Produto"
+    brand.brand_name AS "Marca"
+    products.model_year AS "Ano do modelo"
+FROM 
+     products
+        LEFT JOIN
+     brands
+    on products.brand_id=brands.id
+WHERE
+    products.product_id not in (SELECT distinct product_id from order_items)
